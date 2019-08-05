7a3bdf41f67c9035ca46766417813823daa188df
wimpykatana <wim.katana@gmail.com>
2019-08-05 13:17:03 +0700

    add database name

--- microservice/user/config/db.js
+++ microservice/user/config/db.js
@@ -3,15 +3,15 @@ mongoose.set('useFindAndModify', false);
 
 function con(){
 	mongoose.Promise = global.Promise;
-
-	if(process.env.NODE_ENV === 'production') {
+	console.log("lo dimana",process.env.NODE_ENV);
+	// if(process.env.NODE_ENV === 'production') {
 		// mongoose.connect(`mongodb://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,{ useNewUrlParser: true });
 		mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,{ useNewUrlParser: true });
-	}
-	else {
-		// mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,{ useNewUrlParser: true });
-		mongoose.connect(`mongodb://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,{ useNewUrlParser: true });
-	}
+	// }
+	// else {
+	// 	// mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,{ useNewUrlParser: true });
+	// 	mongoose.connect(`mongodb://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,{ useNewUrlParser: true });
+	// }
 	
 	mongoose.connection
 		.once('open', () => console.log('MongoDB running and connected'))
